5b343907e1ca8b19b9a9177d225662bc79bfd88f
Alex chow <zhouchuanglin@gmail.com>
2023-12-01 12:26:36 +0100

    3. net8 webapp auto 添加  tabledemo

--- WebAppCrud/BlazorWebApp/BlazorWebApp.Client/WeatherForecastService.cs
+++ WebAppCrud/BlazorWebApp/BlazorWebApp.Client/WeatherForecastService.cs
@@ -28,11 +28,14 @@ public class WeatherForecastService
         return forecasts;
     }
 
-    public class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
-        public int TemperatureC { get; set; }
-        public string? Summary { get; set; }
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
+
+}
+
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
