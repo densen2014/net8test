32b1e0b86f9ae45653f7c270c057313113100980
Alex chow <zhouchuanglin@gmail.com>
2023-12-01 18:28:43 +0100

    7. net8 webapp auto 添加 Table Demo with Service

--- WebAppCrud/BlazorWebApp/BlazorWebApp.Client/WeatherForecastService.cs
+++ WebAppCrud/BlazorWebApp/BlazorWebApp.Client/WeatherForecastService.cs
@@ -1,4 +1,7 @@
 ﻿
+using FreeSql.DataAnnotations;
+using System.ComponentModel;
+
 namespace BlazorWebApp.Services;
 
 /// <summary>
@@ -31,11 +34,31 @@ public class WeatherForecastService
 
 }
 
-
+[AutoGenerateClass(Searchable = true, Filterable = true, Sortable = true)] //BB自动建列特性,可搜索,可过滤,可排序
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    [Column(IsIdentity = true)] //FreeSql ORM特性: 自增,第一个名称ID的自动为主键
+    [DisplayName("序号")] //BB使用DisplayName渲染列名
+    public int ID { get; set; }
+
+    [DisplayName("日期")]
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     public int TemperatureC { get; set; }
     public string? Summary { get; set; }
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public static List<WeatherForecast> GenDatas()
+    {
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToArray();
+
+        return forecasts.ToList();
+
+    }
 }
---
+++ WebAppCrud/BlazorWebApp/BlazorWebApp/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWebApp;
+
+/// <summary>
+/// 系统常量
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public const string TableDemoOrm = "TableDemoOrm";
+}
